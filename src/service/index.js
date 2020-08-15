d8eb2d9e7abf850cf6945091c29af4808ac16b94
chetvishal <vishal1982c@gmail.com>
2020-08-15 21:34:22 +0530

    done creating details page

--- src/service/index.js
+++ src/service/index.js
@@ -150,14 +150,62 @@ export const fetchMovieDetail = async (id) => {
     }
 }
 
-export const fetchMovieVideos = () => {
+export const fetchMovieVideos = async (id) => {
+    try{
+        const {data} = await axios.get(`${movieUrl}/${id}/videos`, {
+            params: {
+                api_key: apiKey
+            }
+        })
+        return data['results'][0];
+    }
+    catch (error) {
 
+    }
 }
 
-export const fetchCasts = () => {
+export const fetchCasts = async (id) => {
+    try{
+        const {data} = await axios.get(`${movieUrl}/${id}/credits`, {
+            params: {
+                api_key: apiKey
+            }
+        });
+        const modifiedData = data['cast'].map((c) => ({
+            id: c['cast_id'],
+            character: c['character'],
+            name: c['name'],
+            id: 'https://image.tmdb.org/t/p/w200' + c['profile_path'],
+        }))
+        return modifiedData;
+    }
+    catch (error) {
 
+    }
 }
 
-export const fetchSimilarMovie = () => {
+export const fetchSimilarMovie = async (id) => {
+    try{
+        const {data} = await axios.get(`${movieUrl}/${id}/recommendations`, {
+            params: {
+                api_key: apiKey,
+                language: 'en_US'
+            }
+        })
+        const posterUrl = 'https://image.tmdb.org/t/p/original/';
+        const modifiedData = data['results'].map((m) => ({
+            id: m['id'],
+            backPoster: posterUrl + m['backdrop_path'],
+            popularity: m['popularith'],
+            title: m['title'],
+            poster: posterUrl + m['poster_path'],
+            overview: m['overview'],
+            rating: m['vote_average'],
+        }))
 
+        return modifiedData;
+    }
+    catch (error) {
+
+    }
 }
